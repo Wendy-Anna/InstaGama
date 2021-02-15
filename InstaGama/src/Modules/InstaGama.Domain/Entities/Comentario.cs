347d1fdd8d583917e301b9f92bfd13595750bde7
RRRAMOS <dsraquelramos@gmail.com>
2021-02-14 21:22:41 -0300

    criação da classe comentários

--- InstaGama/src/Modules/InstaGama.Domain/Entities/Comentario.cs
+++ InstaGama/src/Modules/InstaGama.Domain/Entities/Comentario.cs
@@ -4,7 +4,40 @@ using System.Text;
 
 namespace InstaGama.Domain.Entities
 {
-    class Comentario
+   public class Comentario
     {
+
+        public Comentario(int usuarioId, int postagemId, string texto, DateTime data)
+        {
+            UsuarioId = usuarioId;
+            PostagemId = postagemId;
+            Texto = texto;
+            Data = data;
+        }
+
+        public int Id {get; private set; }
+        public int UsuarioId { get; set; }
+        public int PostagemId { get; set; }
+        public string Texto { get; set; }
+        public DateTime Data { get; set; }
+
+
+         public string validaTexto(string texto)
+        {
+            
+            if (texto.Length >=10 && texto.Length <= 300 )
+            {
+
+                return texto;
+
+            }
+
+            else {
+                return ("Capriche num texto de 10 a 300 caracteres");
+                }
+
+
+        }
+               
     }
 }

2f37d453ff032a0d9e0b56e124d54ccb34bc7049
Karol <karolroberta92@hotmail.com>
2021-02-15 10:30:33 -0300

    Criando Interface e Repositório

---
+++ InstaGama/src/Modules/InstaGama.Domain/Interfaces/ICurtidasRepository.cs
@@ -0,0 +1,13 @@
+﻿using InstaGama.Domain.Entities;
+using System.Threading.Tasks;
+
+namespace InstaGama.Domain.Interfaces
+{
+    public interface ICurtidasRepository
+    {
+        Task<int> InsertAsync(Curtidas curtidas);
+        Task DeleteAsync(int id);
+        Task<int> GetQuantityOfCurtidasByPostagemIdAsync(int postagemId);
+        Task<Curtidas> GetByUsuarioIdAndPostagemIdAsync(int usuarioId, int postagemId);
+    }
+}

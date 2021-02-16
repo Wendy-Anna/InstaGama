c75372ad0bc18444153be20123fac9253a3a832b
Luana Cardoso <luanacardosotra@gmai.com>
2021-02-15 21:00:45 -0300

    Entidade de Postagem

---
+++ InstaGama/InstaGama/src/Modules/InstaGama.Domain/Entities/Postagem.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InstaGama.Domain.Entities
+{
+    class Postagem
+    {
+            public Postagem(
+                //Usuario usuarioId,
+                string texto,
+                string media,
+                DateTime dataCriacao)
+            {
+                //UsuarioId = usuarioId
+                Texto = texto;
+                Media = media;
+                DataCriacao = dataCriacao;
+            }
+
+            public int Id { get; private set; }
+
+            public Usuario UsuarioId { get; private set; }
+
+            public string Texto { get; private set; }
+
+            public string Media { get; private set; }
+
+            public DateTime DataCriacao { get; private set; }
+
+
+            public bool isvalid()
+            {
+                bool valido = true;
+                if (string.IsNullOrEmpty(Texto))
+                {
+                    valido = false;
+                }
+                return valido;
+            }
+
+            public void SetId(int id)
+            {
+                Id = id;
+            }
+        }
+
+}

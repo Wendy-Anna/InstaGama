6c2f90651451a238feac72638097def2d62f967e
RRRAMOS <dsraquelramos@gmail.com>
2021-02-15 22:04:31 -0300

    criação interface comentario

---
+++ InstaGama/src/Modules/InstaGama.Domain/Interfaces/IComentarioRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InstaGama.Domain.Interfaces
+
+    public interface IComentarioRepository
+    {
+
+        Task <int> InsertAsync(Comentarios comentario);
+        Task <List<Comentarios>> GetByPostagemIdAsync(int postagemId);
+
+    }
+}

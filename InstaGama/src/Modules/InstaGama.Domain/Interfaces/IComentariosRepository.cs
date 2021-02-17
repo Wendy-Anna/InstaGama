ee57a8888eaca5b7fd225008b6dcb152cfb63aba
RRRAMOS <dsraquelramos@gmail.com>
2021-02-16 23:26:35 -0300

    criação de interface comentários com métodos de repositório

---
+++ InstaGama/src/Modules/InstaGama.Domain/Interfaces/IComentariosRepository.cs
@@ -0,0 +1,14 @@
+﻿using InstaGama.Domain.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace InstaGama.Domain.Interfaces
+{
+    public interface IComentarioRepository
+    {
+
+        Task<int> InsertAsync(Comentario comentario);
+        Task<List<Comentario>> GetByPostagemIdAsync(int postagemId);
+
+    }
+}

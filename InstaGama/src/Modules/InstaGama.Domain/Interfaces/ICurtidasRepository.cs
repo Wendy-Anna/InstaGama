feaf76eec0d7734bf26df8fc4c23410fc3a0566c
MarciaMartins <marciapmartins@gmail.com>
2021-02-18 00:29:43 +0000

    Adequação das classes Curtidas, ICutidasRepository e CurtidasRepository

--- InstaGama/src/Modules/InstaGama.Domain/Interfaces/ICurtidasRepository.cs
+++ InstaGama/src/Modules/InstaGama.Domain/Interfaces/ICurtidasRepository.cs
@@ -5,9 +5,9 @@ namespace InstaGama.Domain.Interfaces
 {
     public interface ICurtidasRepository
     {
-        Task<int> InserirAsync(Curtidas curtidas);
+        Task<int> InserirAsync(Curtida curtidas);
         Task ApagarAsync(int id);
         Task<int> PegarQuantidadeCurtidasIdAsync(int postagemId);
-        Task<Curtidas> PegarUsuarioIdEPostagemIdAsync(int usuario, int postagemId);
+        Task<Curtida> PegarUsuarioIdEPostagemIdAsync(int usuario, int postagemId);
     }
 }

cb51e83186314a2b20a8300d6437619c11c5674c
Isabella Nunes <isabella.nunes@philips.com>
2021-02-19 20:04:25 -0300

    Changed some names

---
+++ InstaGama/src/Modules/InstaGama.Application/AppComentario/ComentarioAppService.cs
@@ -0,0 +1,12 @@
+﻿using InstaGama.Application.AppComentario.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InstaGama.Application.AppComentario
+{
+    class ComentarioAppService : IComentarioAppService
+    {
+        
+    }
+}
---
+++ InstaGama/src/Modules/InstaGama.Application/AppComentario/Interface/IComentarioAppService.cs
@@ -0,0 +1,16 @@
+﻿using InstaGama.Application.AppComentario.Input;
+using InstaGama.Application.AppComentario.Output;
+using InstaGama.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InstaGama.Application.AppComentario.Interface
+{
+    class IComentarioAppService
+    {
+        
+
+    }
+}
